27148faec9319d5742db4aed890b838306d526ec
Alyssa <50183184+ampettit@users.noreply.github.com>
2020-06-22 18:25:05 -0500

    Update CarNerdMainPage.java

--- CarNerdMainPage.java
+++ CarNerdMainPage.java
@@ -1,8 +1,8 @@
-package components;
+package CarNerd;
 
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
@@ -10,103 +10,90 @@ import javax.swing.JComponent;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Label;
-import java.awt.TextArea;
-import java.awt.event.KeyEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 
- 
 public class Cant extends JPanel {
     public Cant() {
-        super(new GridLayout(1, 1));
-         
+    	super(new GridLayout(1, 1));
+        
+
         JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.setTabPlacement(JTabbedPane.BOTTOM);
+        tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
         ImageIcon icon = createImageIcon("");
-         
+
         JComponent panel1 = makeTextPanel("Panel #1");
         tabbedPane.addTab("Car", icon, panel1,
                 "Does nothing");
-        panel1.setPreferredSize(new Dimension(1000,1000));
+        panel1.setPreferredSize(new Dimension(300,300));
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
-         
+        
+    // INFO TAB - ALYSSA
         JComponent panel2 = makeTextPanel("Panel #2");
+        // Makes it that it doesn't take up the entire area.  Still adjusting
+        panel2.setLayout(new FlowLayout());
         tabbedPane.addTab("Info", icon, panel2,
                 "Does twice as much nothing");
+        
+        JComponent tbCarName = new JTextField("Enter Name");
+        panel2.add(tbCarName);
+        tbCarName.setSize(200, 20);
+        JComponent tbCarYear = new JTextField("Enter Year");
+        panel2.add(tbCarYear);
+        
+        JComponent tbCarMake = new JTextField("Enter Make");
+        panel2.add(tbCarMake);
+        
+        JComponent tbCarModel = new JTextField("Enter Model");
+        panel2.add(tbCarModel);
+        
+        JComponent tbCarTrim = new JTextField("Enter Trim if Applicable");
+        panel2.add(tbCarTrim);
+        
+        JComponent tbCarVIN = new JTextField("Enter VIN");
+        panel2.add(tbCarVIN);
+        
+        JComponent carPlateNum = new JTextField("Enter Plate Number");
+        panel2.add(carPlateNum);
+        
         tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-         
+
         JComponent panel3 = makeTextPanel("Panel #3");
         tabbedPane.addTab("Log", icon, panel3,
                 "Still does nothing");
         tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
-         
+
         JComponent panel4 = makeTextPanel("Panel 4");
-        tabbedPane.addTab("Mileage", icon, panel4,
-                "Does nothing at all");
+        tabbedPane.addTab("Mileage", icon, panel4,"Does nothing at all");
         tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
-         
+
         JComponent panel5 = makeTextPanel("Panel #5");
         tabbedPane.addTab("Reminder", icon, panel5,
                 "Still does nothing");
-        tabbedPane.setMnemonicAt(4, KeyEvent.VK_5);
-        
-        JComponent panel6 = makeTextPanel("Panel #6");
+        //tabbedPane.setMnemonicAt(4, KeyEvent.VK_5);
 
-        panel6.setLayout(new FlowLayout());
+        JComponent panel6 = makeTextPanel("Panel #6");
         tabbedPane.addTab("Documents", icon, panel6,
                 "Still does nothing");
-        tabbedPane.setMnemonicAt(5, KeyEvent.VK_6);
-        
+        //tabbedPane.setMnemonicAt(5, KeyEvent.VK_6);
 
-        JButton btnInsurance = new JButton("Insurance");
-        btnInsurance.setFont(new Font("Tahoma", Font.BOLD, 13));
-        btnInsurance.setForeground(new Color(0, 0, 205));
-        btnInsurance.setBounds(12, 109, 118, 25);
-        panel6.add(btnInsurance);
-        
-        JButton btnRegistration = new JButton("Registration");
-        btnRegistration.setFont(new Font("Tahoma", Font.BOLD, 13));
-        btnRegistration.setForeground(new Color(0, 0, 205));
-        btnRegistration.setBounds(12, 206, 118, 25);
-        panel6.add(btnRegistration);
-        
-        JButton btnLicense = new JButton("License");
-        btnLicense.setFont(new Font("Tahoma", Font.BOLD, 13));
-        btnLicense.setForeground(new Color(0, 0, 205));
-        btnLicense.setBounds(12, 156, 118, 25);
-        panel6.add(btnLicense);
-        
         JComponent panel7 = makeTextPanel("Panel #7");
         tabbedPane.addTab("Settings", icon, panel7,
                 "Still does nothing");
         tabbedPane.setMnemonicAt(6, KeyEvent.VK_7);
-        
+
         JComponent panel8 = makeTextPanel("Panel #8");
         tabbedPane.addTab("Quit", icon, panel8,
                 "Still does nothing");
         tabbedPane.setMnemonicAt(7, KeyEvent.VK_8);
-        
-
-        //Add the tabbed pane to this panel.
-        add(tabbedPane);
-         
-        //The following line enables to use scrolling tabs.
 
 
         add(tabbedPane);
-         
-
-        tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
     }
-     
+
     protected JComponent makeTextPanel(String text) {
         JPanel panel = new JPanel(false);
         JLabel filler = new JLabel(text);
@@ -115,9 +102,7 @@ public class Cant extends JPanel {
         panel.add(filler);
         return panel;
     }
-     
 
-    /** Returns an ImageIcon, or null if the path was invalid. */
 
     protected static ImageIcon createImageIcon(String path) {
         java.net.URL imgURL = Cant.class.getResource(path);
@@ -128,41 +113,20 @@ public class Cant extends JPanel {
             return null;
         }
     }
-     
-
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from
-     * the event dispatch thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("CarNerd");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         
-        //Add content to the window.
-        frame.add(new Cant(), BorderLayout.CENTER);
-         
-        //Display the window.
 
 
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("CarNerd");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.getContentPane().add(new Cant(), BorderLayout.CENTER);
 
-        frame.add(new Cant(), BorderLayout.CENTER);
 
         frame.pack();
         frame.setVisible(true);
     }
-     
-    public static void main(String[] args) {
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                //Turn off metal's use of bold fonts
 
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
         UIManager.put("swing.boldMetal", Boolean.FALSE);
