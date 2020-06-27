afc6705f1967def59ff34bea11b715151c65e9ff
Alyssa <50183184+ampettit@users.noreply.github.com>
2020-06-26 20:19:43 -0500

    Update CarNerdMainPage.java

--- CarNerdMainPage.java
+++ CarNerdMainPage.java
@@ -25,8 +25,8 @@ import java.awt.Panel;
 import java.awt.event.KeyEvent;
 
 @SuppressWarnings("serial")
-public class testing extends JPanel {
-    public testing() {
+public class CarNerdMainPage extends JPanel {
+    public CarNerdMainPage() {
     	super(new GridLayout(1, 1));
 
 
@@ -34,119 +34,128 @@ public class testing extends JPanel {
         tabbedPane.setTabPlacement(JTabbedPane.BOTTOM);
         tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
         ImageIcon icon = createImageIcon("");
-
+// ****BACKGROUND COLORS FOR TABS******        
+        int r = 191, g = 136, b = 187;
         
 // ******CAR TAB******
-        JComponent panel1 = makeTextPanel("Panel #1");
-        tabbedPane.addTab("Car", icon, panel1,
-                "Does nothing");
-        panel1.setPreferredSize(new Dimension(300,300));
-        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+        JComponent panel0 = makeTextPanel("Panel #0");
+        tabbedPane.addTab("Car", icon, panel0, null);
+        panel0.setBackground(new Color(r, g, b));
+        panel0.setPreferredSize(new Dimension(900, 900));
+        tabbedPane.setMnemonicAt(0, KeyEvent.VK_0);
         
         
 // ******INFO TAB****** - ALYSSA
-        JComponent panel2 = makeTextPanel("Panel #2");
+        JComponent panel1 = makeTextPanel("Panel #1");
         // Makes it that it doesn't take up the entire area.  Still adjusting
-        panel2.setLayout(new FlowLayout());
-        tabbedPane.addTab("Info", icon, panel2,
-                "Does twice as much nothing");
+        //panel1.setLayout(new FlowLayout());
+        tabbedPane.addTab("Info", null, panel1, null);
+        panel1.setBackground(new Color(r, g, b));
+        panel1.setLayout(null);
         
-        JComponent tbCarName = new JTextField("Enter Name");
-        panel2.add(tbCarName);
-        tbCarName.setSize(200, 20);
-        JComponent tbCarYear = new JTextField("Enter Year");
-        panel2.add(tbCarYear);
-        
-        JComponent tbCarMake = new JTextField("Enter Make");
-        panel2.add(tbCarMake);
-        
-        JComponent tbCarModel = new JTextField("Enter Model");
-        panel2.add(tbCarModel);
-        
-        JComponent tbCarTrim = new JTextField("Enter Trim if Applicable");
-        panel2.add(tbCarTrim);
-        
-        JComponent tbCarVIN = new JTextField("Enter VIN");
-        panel2.add(tbCarVIN);
-        
-        JComponent carPlateNum = new JTextField("Enter Plate Number");
-        panel2.add(carPlateNum);
-        
-
+        JLabel lblCarName = new JLabel("Name");
+        JLabel lblCarYear = new JLabel("Year");
+		JLabel lblCarMake = new JLabel("Make");
+        JLabel lblCarModel = new JLabel("Model");
+        JLabel lblCarTrim = new JLabel("Trim if Applicable");
+		JLabel lblCarVIN = new JLabel("VIN");
+        JLabel lblPlateNum = new JLabel("Plate Number");
         JButton btnEdit = new JButton("Edit");
+        
+        
         btnEdit.setFont(new Font("Tahoma", Font.BOLD, 13));
         btnEdit.setForeground(new Color(0, 0, 205));
-        btnEdit.setBounds(12, 206, 118, 25);
-        panel2.add(btnEdit);
         
-        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+        
+        lblCarName.setBounds(10, 50, 500, 14);
+		lblCarYear.setBounds(10, 70, 0, 0);
+        lblCarMake.setBounds(170, 70, 118, 25);
+        lblCarModel.setBounds(270, 70, 118, 25);
+		lblCarTrim.setBounds(370, 70, 118, 25);
+        lblCarVIN.setBounds(10, 90, 118, 25);
+        lblPlateNum.setBounds(170, 90, 118, 25);
+        btnEdit.setBounds(400, 200, 118, 25);
+        
+        panel1.add(lblCarName);
+		panel1.add(lblCarYear);
+        panel1.add(lblCarMake);
+        panel1.add(lblCarModel);
+        panel1.add(lblCarTrim);
+        panel1.add(lblCarVIN);
+        panel1.add(lblPlateNum);
+        panel1.add(btnEdit);
+        
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_1);
 
 // ******LOG TAB******
-        JComponent panel3 = makeTextPanel("Panel #3");
-        Panel panel = new Panel();
-        tabbedPane.addTab("Log", null, panel, null);
-        panel.setLayout(null);
-        
+        JComponent panel2 = makeTextPanel("Panel #2");
+        //Panel panel2 = new Panel();
+        tabbedPane.addTab("Log", null, panel2, null);
+        panel2.setLayout(null);
+        panel2.setBackground(new Color(r, g, b));
         JLabel lblNewLabel = new JLabel("NAME, YEAR MAKE MODEL: CURRENT CAR");
-        lblNewLabel.setBounds(55, 11, 500, 14);
-        panel.add(lblNewLabel);
+        lblNewLabel.setBounds(55, 15, 500, 14);
+        panel2.add(lblNewLabel);
         
         JTextArea txtrEnterTextHere = new JTextArea();
         txtrEnterTextHere.setText("Enter text here");
         txtrEnterTextHere.setBounds(222, 97, 407, 241);
-        panel.add(txtrEnterTextHere);
+        panel2.add(txtrEnterTextHere);
         
         JTextField txtTitle = new JTextField();
         txtTitle.setText("Title");
         txtTitle.setBounds(196, 66, 86, 20);
-        panel.add(txtTitle);
+        panel2.add(txtTitle);
         txtTitle.setColumns(10);
         
         JTextField txtEvent = new JTextField();
         txtEvent.setText("Event");
         txtEvent.setColumns(10);
         txtEvent.setBounds(379, 66, 86, 20);
-        panel.add(txtEvent);
+        panel2.add(txtEvent);
         
         JTextField txtRemindDate = new JTextField();
         txtRemindDate.setText("Remind Date");
         txtRemindDate.setColumns(10);
         txtRemindDate.setBounds(569, 66, 86, 20);
-        panel.add(txtRemindDate);
+        panel2.add(txtRemindDate);
         
-        JButton btnNewButton = new JButton("SAVE");
-        btnNewButton.setBounds(268, 349, 89, 45);
-        panel.add(btnNewButton);
+        JButton btnSave = new JButton("SAVE");
+        btnSave.setBounds(268, 349, 89, 45);
+        panel2.add(btnSave);
         
-        JButton btnCancel = new JButton("CANCEL");
-        btnCancel.setBounds(501, 349, 89, 45);
-        panel.add(btnCancel);
-        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);        
+        JButton btnClear = new JButton("CLEAR");
+        btnClear.setBounds(501, 349, 89, 45);
+        panel2.add(btnClear);
+              
  
         String[] reminders = {"Reminder1", "Reminder2", "Reminder3", "Reminder4", "Reminder5", "Reminder6", "Reminder7"};
-        panel3.setLayout(new BorderLayout());	
+        //panel2.setLayout(new BorderLayout());	
 		JList list = new JList(reminders);
 		JScrollPane scrollPane = new JScrollPane(list);
-		panel3.add(scrollPane, BorderLayout.WEST);
-		scrollPane.setPreferredSize(new Dimension(250, 80));
+		panel2.add(scrollPane, BorderLayout.WEST);
+		
+		scrollPane.setBounds(10, 40, 170, 400);
+		//scrollPane.setPreferredSize(new Dimension(100, 80));
+		/*
 		scrollPane.setMaximumSize(new Dimension(360, 120));
 		scrollPane.setSize(new Dimension(250, 80));
-		scrollPane.revalidate();
+		*/
+		//scrollPane.revalidate();
 		
-		
+		tabbedPane.setMnemonicAt(2, KeyEvent.VK_2);  
 		
 // ******MILEAGE TAB******        
-        JComponent panel4 = makeTextPanel("Panel 4");
-        tabbedPane.addTab("Mileage", icon, panel4,"Does nothing at all");
-        tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);      
+        JComponent panel3 = makeTextPanel("Panel #3");
+        tabbedPane.addTab("Mileage", icon, panel3, null);
+        tabbedPane.setMnemonicAt(3, KeyEvent.VK_3);      
         
 
         
 // ******REMINDER TAB - BEING MERGED WITH LOG******        
         //JComponent panel5 = makeTextPanel("Panel #5");
         //String[] reminders = {"Reminder1", "Reminder2", "Reminder3", "Reminder4", "Reminder5", "Reminder6", "Reminder7"};
-        //tabbedPane.addTab("Reminders", icon, panel5,
-        //        "Reminders");
+        //tabbedPane.addTab("Reminders", icon, panel5, "Reminders");
         //tabbedPane.setMnemonicAt(4, KeyEvent.VK_5);
         //panel5.setLayout(new BorderLayout());	
 		//JList list = new JList(reminders);
@@ -159,25 +168,23 @@ public class testing extends JPanel {
 
 		
 // ******SETTINGS TAB******        
-      /*  
         JComponent panel6 = makeTextPanel("Panel #6");
-        tabbedPane.addTab("Settings", icon, panel6,
-                "Still does nothing");
+        tabbedPane.addTab("Settings", icon, panel6, null);
         tabbedPane.setMnemonicAt(4, KeyEvent.VK_6);
         add(tabbedPane);
-        */
+
         
         
 // ******EXTRA TAB - FOR DEVELOPMENT******
-       
-        JComponent panel6 = makeTextPanel("Panel #6");
-        tabbedPane.addTab("Settings", icon, panel6, "Still does nothing");
+      /*         
+        JComponent panel6 = makeTextPanel("Panel #4");
+        tabbedPane.addTab("Settings", icon, panel6, null);
         
-        tabbedPane.setMnemonicAt(4, KeyEvent.VK_6);
+        tabbedPane.setMnemonicAt(4, KeyEvent.VK_4);
+        
+        */        
+// ******APPLIES TO ENTIRE WINDOW******
         add(tabbedPane);
-        
-        
-        
         
     }
 
@@ -191,7 +198,7 @@ public class testing extends JPanel {
     }
 
     protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = testing.class.getResource(path);
+        java.net.URL imgURL = CarNerdMainPage2.class.getResource(path);
         if (imgURL != null) {
             return new ImageIcon(imgURL);
         } else {
@@ -205,7 +212,7 @@ public class testing extends JPanel {
         JFrame frame = new JFrame("CarNerd");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.getContentPane().add(new testing(), BorderLayout.CENTER);
+        frame.getContentPane().add(new CarNerdMainPage2(), BorderLayout.CENTER);
 
         frame.pack();
         frame.setVisible(true);
