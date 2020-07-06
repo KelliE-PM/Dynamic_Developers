a5b7d2274279f6cf3830ae2403a5938f076a02bd
8i11y79 <66650246+8i11y79@users.noreply.github.com>
2020-07-06 14:49:49 -0500

    Add files via upload

--- popUp.java
+++ popUp.java
@@ -1,8 +1,6 @@
 package Program;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -12,26 +10,40 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
+import java.awt.Font;
+import java.util.Date;
 
 public class popUp extends JFrame {
-
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField txtMmddyyyy;
-	private JCheckBox chckbxNewCheckBox;
-	private JCheckBox chckbxNewCheckBox_1;
-	private JLabel lblNewLabel_2;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
-
-	/**
-	 * Launch the application.
-	 */
+	private JTextField newMileage;
+	private JTextField newDate;
+	private JCheckBox checkChange;
+	private JCheckBox checkSynth;
+	private JLabel nextChange;
+	private JButton update;
+	private JButton exit;
+	private JLabel message;
+	private JButton reset;
+	private JLabel last;
+	
+	/* not finished
+	 
+	 public static void mile() {
+		int miles;
+		Date date;
+	}
+	
+	public static void change() {
+		int changeMiles;
+		Date changeDate;
+		boolean synthetic;
+	}
+	*/
+	
 	public static void main(String[] args) {
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -44,49 +56,143 @@ public class popUp extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public popUp() {
+		
+		// Last given mileage   FIX  can't initialize empty every time
+		int lastMiles = 0;
+		String lastDate = "MM/DD/YYYY";
+		
+		// Update mileage (starting with last given in box)
+		int currentMiles = lastMiles;
+		String currentDate = lastDate;
+		
+		// Oil change
+		int changeMiles;
+		String changeDate;
+		
+		// Next oil change
+		String nextChangeDate;  // add 6 months
+		int nextChangeMiles;
+		int milesLeft;
+		
+		// If true, mileage updates oil change label
+		boolean change = false;
+		boolean synthetic = false;
+		String syn = "regular";
+		if (synthetic == true) {syn = "synthetic";}
+		
+		// Dummy info
+		lastMiles = 564378;
+		lastDate = "12/31/2020";
+		currentMiles = lastMiles;
+		changeMiles = 562934;
+		changeDate = "12/12/2020";
+		nextChangeDate = "6/12/2021";
+		
+		// Save mileage for reset
+		int saveMiles = lastMiles;
+		String saveDate = lastDate;
+		int saveChangeMiles = changeMiles;
+		String saveChangeDate = changeDate;
+		
+		// Calculates next change and miles left
+		nextChangeMiles = changeMiles + 3000;
+		if (synthetic == true) {nextChangeMiles = changeMiles + 5000;} 
+		// int nextChangeDate = changeDate + 6 months
+		// if (synthetic == true) nextChangeDate = changeDate + 9 months
+		milesLeft = nextChangeMiles - currentMiles;
+		
+		// Message at bottom (can change)
+		String due = "             In " + milesLeft + " miles or by " + nextChangeDate;
+		if (currentMiles > nextChangeMiles) /*|| currentDate > changeDate) */ {due = "             You are past due for an oil change";}
+		
+
+
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 360, 195);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[][54.00][][53.00,left]", "[][][]"));
+		contentPane.setLayout(new MigLayout("", "[][58.00][][60.00,left]", "[][][][][]"));
 		
-		JLabel lblNewLabel = new JLabel("ENTER NEW MILEAGE:  ");
-		contentPane.add(lblNewLabel, "cell 0 0,alignx trailing");
+		JLabel enter = new JLabel("ENTER NEW MILEAGE:  ");
+		contentPane.add(enter, "cell 0 0,alignx trailing");
 		
-		textField = new JTextField();
-		textField.setHorizontalAlignment(SwingConstants.LEFT);
-		textField.setText("0000000");
-		contentPane.add(textField, "cell 1 0,alignx center");
-		textField.setColumns(10);
+		newMileage = new JTextField();
+		newMileage.setHorizontalAlignment(SwingConstants.LEFT);
+		newMileage.setText(String.valueOf(lastMiles));
+		contentPane.add(newMileage, "cell 1 0,alignx center");
+		newMileage.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel(" DATE: ");
-		contentPane.add(lblNewLabel_1, "cell 2 0,alignx trailing");
+		// setMileage = 
 		
-		txtMmddyyyy = new JTextField();
-		txtMmddyyyy.setText("mm/dd/yyyy");
-		contentPane.add(txtMmddyyyy, "cell 3 0,alignx left,aligny center");
-		txtMmddyyyy.setColumns(10);
+		JLabel date = new JLabel(" DATE: ");
+		contentPane.add(date, "cell 2 0,alignx trailing");
 		
-		chckbxNewCheckBox = new JCheckBox("Oil Change?");
-		contentPane.add(chckbxNewCheckBox, "cell 0 1,alignx right");
+		newDate = new JTextField();
+		newDate.setText(lastDate);
+		contentPane.add(newDate, "cell 3 0,alignx center,aligny center");
+		newDate.setColumns(10);
+
+		// setDate = 
 		
-		chckbxNewCheckBox_1 = new JCheckBox("Synthetic oil?");
-		contentPane.add(chckbxNewCheckBox_1, "cell 1 1 2 1,growx");
+		last = new JLabel("             Last oil change was on " + changeDate + " with " + syn + " oil");
+		last.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		contentPane.add(last, "cell 0 1 4 1,growx");
 		
-		btnNewButton = new JButton("Update");
-		contentPane.add(btnNewButton, "cell 3 1");
+		checkChange = new JCheckBox("Oil Change?");
+		contentPane.add(checkChange, "cell 0 2,alignx right,aligny top");
 		
-		lblNewLabel_2 = new JLabel("            Next oil change at 0003000 miles    ");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
-		contentPane.add(lblNewLabel_2, "cell 0 2 3 1,alignx left");
+		if (checkChange.isSelected()) {change = true;} 
+		else{ change = false;}
 		
-		btnNewButton_1 = new JButton("   Exit   ");
-		contentPane.add(btnNewButton_1, "cell 3 2");
+		checkSynth = new JCheckBox("Synthetic oil?");
+		contentPane.add(checkSynth, "cell 1 2 2 1,growx,aligny top");
+		
+		if (checkSynth.isSelected()) {synthetic = true;} 
+		else{ synthetic = false;}
+		
+		update = new JButton("Update");
+		contentPane.add(update, "cell 3 2");
+		
+		/*
+		currentMiles = setMiles;
+		currentDate = setDate;
+		// mile(currentMiles, currentDate);
+		// update message
+		 
+		if (change == true) {changeMiles = currentMiles; changeDate = currentDate;} 
+		// change(changeMiles, changeDate, synthetic);
+		// update last label and message
+		*/
+				
+		// Errors
+		// if (currentMiles < lastMiles) {currentMiles = saveMiles;}  // mileage won't update if less than old mileage.
+		// if (currentDate < lastDate) {currentDate = saveDate}  // date won't update if earlier than old date.
+		
+		nextChange = new JLabel("             Next oil change at " + nextChangeMiles + " miles    ");
+		nextChange.setHorizontalAlignment(SwingConstants.LEFT);
+		contentPane.add(nextChange, "cell 0 3 3 1,alignx left,aligny bottom");
+		
+		reset = new JButton("Reset");
+		contentPane.add(reset, "cell 3 3,growx,aligny center");
+		
+		/* 
+		lastMiles = saveMiles;
+		lastDate = saveDate;
+		changeMiles = saveChangeMiles;
+		changeDate = saveChangeDate;
+		*/
+		
+		message = new JLabel(due);
+		contentPane.add(message, "cell 0 4 3 1,alignx left,aligny top");
+		
+		exit = new JButton("   Exit   ");
+		contentPane.add(exit, "cell 3 4,growy");
+		
+		// mile(currentMiles, currentDate)
+		// popUp.dispose();
 	}
 
 }
