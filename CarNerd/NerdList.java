20ccfe6e5aa51adeb9737b09c8ae828a10665396
8i11y79 <66650246+8i11y79@users.noreply.github.com>
2020-07-15 20:05:55 -0500

    Update NerdList.java
    added mileage list

--- CarNerd/NerdList.java
+++ CarNerd/NerdList.java
@@ -4,5 +4,7 @@ import java.util.ArrayList;
 
 public class NerdList {
 	static ArrayList<Car> listCars = new ArrayList<Car>();
+	static ArrayList<Mileage> listMiles = new ArrayList<Mileage>();
+	
 	NerdList(){}
 }
